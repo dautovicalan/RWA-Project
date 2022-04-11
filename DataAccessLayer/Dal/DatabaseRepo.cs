4ad380f1ca0d6248d03985655c82777891a0ee55
RealMeKaNiK <dautovic2000@gmail.com>
2022-04-11 15:49:53 +0200

    adding connection string

---
+++ DataAccessLayer/Dal/DatabaseRepo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccessLayer.Dal
+{
+    internal class DatabaseRepo : IRepo
+    {
+        public void CreateApartment()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateTag()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteApartmentById(int aparmentId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteTagById(int tagId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetApartmentById(int apartmentId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<string> GetApartments()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<string> GetTags()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateApartmentById(int apartmentId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Dal/IRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Dal
+{
+    public interface IRepo
+    {
+        IList<string> GetApartments();
+        void GetApartmentById(int apartmentId);      
+        void CreateApartment();
+        void UpdateApartmentById(int apartmentId);
+        void DeleteApartmentById(int aparmentId);
+        IList<string> GetTags();
+        void DeleteTagById(int tagId);
+        void CreateTag();
+
+    }
+}
